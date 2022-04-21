8f3b88c41eb0c16f2a7030f83873aba9c00760d5
최정필 <wjdvlf1407@gmail.com>
2022-04-21 23:32:00 +0900

    x,y 좌표 입력

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -1,9 +1,34 @@
 package com.javaex.ex04;
 
+
 public class Point {
 	
+	
+	//필드
 	private int x;
 	private int y;
 	
+	//set
+	public void setNum1(int ppp) {
+		x = ppp;
+	}
+
+
+	public void setNum2(int ooo) {
+		y = ooo;
+
+	}
+	
+	
+	//get	여기서 get값을 PointApp으로 넘기나
+	public int getNum1() {
+		return x;
+		
+	}
+
+	public int getNum2() {
+		return y;
+		
+	}
 
 }
--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -2,5 +2,30 @@ package com.javaex.ex04;
 
 public class PointApp {
 	
+	public static void main(String[] args) {
+		
+		Point num1 =new Point();
+		num1.setNum1((int)(Math.random()*100)+1);
+		num1.setNum2((int)(Math.random()*100)+1);
+		
+		Point num2 =new Point();
+		num2.setNum1((int)(Math.random()*100)+1);
+		num2.setNum2((int)(Math.random()*100)+1);
+		
+		
+		
+		int num1x = num1.getNum1();
+		
+		int num2y = num2.getNum2();
+		
+		
+		System.out.println("점[x="+num1x+", y="+num2y+"]을 그렸습니다.");
+		System.out.println("점[x="+num1x+", y="+num2y+"]을 그렸습니다.");
+		
+		
+		
+		
+		
+	}
 
 }
