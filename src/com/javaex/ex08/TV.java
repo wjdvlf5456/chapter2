26f4e1b65ecd4596cc5355c87d65948172f51a34
최정필 <wjdvlf1407@gmail.com>
2022-04-22 18:15:37 +0900

    TV 채널조정

--- src/com/javaex/ex08/TV.java
+++ src/com/javaex/ex08/TV.java
@@ -11,6 +11,17 @@ public class TV {
 	public TV() {
 
 	}
+	
+	public TV(int channel) {
+		if (channel<1) {
+			this.channel = 255;
+		} else if (channel>255){
+			this.channel = 1;
+		} else {
+			this.channel=channel;
+		}
+		
+	}
 
 	public TV(int channel, int volume, boolean power) {
 		this.channel = channel;
@@ -19,6 +30,17 @@ public class TV {
 	}
 
 	// 메소드 - gs
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+	public void setPower(boolean power) {
+		this.power = power;
+	}
 
 	public int getChannel() {
 		return channel;
@@ -32,26 +54,41 @@ public class TV {
 		return power;
 	}
 
+
+
 	// 메소드 - 일반
+	/*
+ 	
+	public void power(boolean power) {
+		
+	}
+	
 	public void channel(int channel) {
-
+	
 	}
-
+	
 	public void channel(boolean up) {
-
+		
+	
 	}
-
+	
 	public void volume(int volume) {
-
+	
 	}
-
+	
 	public void volume(boolean up) {
-
+	
 	}
-
+	*/
 	//정보출력
+	@Override
+	public String toString() {
+		return "TV [channel=" + channel + ", volume=" + volume + ", power=" + power + "]";
+	}
+	
 	public void status() {
-
+		System.out.println("채널: "+channel+"\t볼륨: "+volume+" 입니다.");
+	
 	}
 
 }
--- src/com/javaex/ex08/TVApp.java
+++ src/com/javaex/ex08/TVApp.java
@@ -1,15 +1,34 @@
 package com.javaex.ex08;
 
+import java.util.Scanner;
+
 public class TVApp {
 	
 	public static void main(String[] args) {
 		
-		TV tv = new TV(7,20,false);
+			TV tv = new TV();
+			tv.setChannel(7);
+			tv.setVolume(20);
+			tv.setPower(false);
+			
+			
+			
+			
+			
+			tv.status();
 		
-		tv.getChannel();
 		
+			
+			
+			System.out.println(tv.toString());
+			
+			
+			
+			
+			
+			
+			
 		
-		tv.status();
 	}
 
 }
