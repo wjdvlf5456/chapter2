2fc369c09b8718f3f910dda74b5819c580bf1ace
최정필 <wjdvlf1407@gmail.com>
2022-04-23 17:30:30 +0900

    TV완성

--- src/com/javaex/ex08/TV.java
+++ src/com/javaex/ex08/TV.java
@@ -9,17 +9,18 @@ public class TV {
 
 	// 생성자
 	public TV() {
-			
-	}
-	 
-	
-	public TV(int channel, int volume, boolean power) {
-		this.channel=channel;
-		this.volume=volume;
-		this.power=power;
 
 	}
-	
+
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+		System.out.println("채널: " + getChannel());
+		System.out.println("볼륨: " + getVolume());
+		System.out.println("전원: " + power);
+
+	}
 
 	// 메소드 - gs
 
@@ -31,69 +32,78 @@ public class TV {
 		return volume;
 	}
 
-
 	// 메소드 - 일반]
+	public void power(boolean on) {
+
+		if (on = true) {
+			this.power = true;
+		} else {
+			this.power = false;
+		}
+	}
+
 	public void channel(int channel) {
-		//1에서 255유지
-		if (channel<1) {
+		// 1에서 255유지
+		if (channel < 1) {
 			this.channel = 255;
-		} else if (channel>255){
+		} else if (channel > 255) {
 			this.channel = 1;
 		} else {
-			this.channel=channel;
+			this.channel = channel;
+		}
+	}
+
+	public void channel(boolean up) {
+		// 1~255유지, 1씩 증감
+		if (up = true) {
+			this.channel = channel + 1;
+			if (channel > 255) {
+				this.channel = 1;
+			}
+		} else {
+			this.channel = channel - 1;
+			if (channel < 1) {
+				this.channel = 255;
+			}
+		}
+	}
+
+	public void volume(int volume) {
+		// 0에서 100유지
+		if (volume < 1) {
+			this.volume = 0;
+		} else if (volume > 100) {
+			this.volume = 100;
+		} else {
+			this.volume = volume;
+		}
+	}
+
+	public void volume(boolean up) {
+		// 1~100유지 1씩 증감
+		if (up = true) {
+			this.volume = volume + 1;
+			if (volume > 100) {
+				this.volume = 100;
+			}
+		} else {
+			this.volume = volume - 1;
+			if (volume < 1) {
+				this.volume = 0;
+			}
 		}
 
 	}
-	
-	public void volume(int volume) {
-		//0에서 100유지
-		if (volume<1) {
-			this.volume=0;
-		} else if(volume>100){
-			this.volume=100;
-		} else {
-			this.volume=volume;
-		}
-	}
-	
- 	
-	public void power(boolean on) {
-		
-		if (on=true) {
-			this.power=true;
-		} else {
-			this.power=false;
-		}
-	}
-	
-	public void channel(boolean up) {
-		//1~255유지, 1씩 증감
-		if (up=true) {
-			this.channel = channel+1;
-		} else {
-			this.channel = channel-1;
-		}
-	}
-	
-	public void volume(boolean up) {
-		//1~100유지 1씩 증감
-		if (up=true) {
-			this.volume = volume+1;
-		} else {
-			this.volume = volume-1;
-		}
-	
-	}
-	
-	//정보출력
+
+	// 정보출력
 	@Override
 	public String toString() {
 		return "TV [channel=" + channel + ", volume=" + volume + ", power=" + power + "]";
 	}
-	
+
 	public void status() {
-		System.out.println("채널: "+channel+"번"+"\t볼륨: "+volume+" 입니다.");
-	
+		System.out.println("채널: " + channel + "번" + "\t볼륨: " + volume + " 입니다.");
+
 	}
 
 }
--- src/com/javaex/ex08/TVApp.java
+++ src/com/javaex/ex08/TVApp.java
@@ -1,31 +1,33 @@
 package com.javaex.ex08;
 
-import java.util.Scanner;
-
 public class TVApp {
 	
 	public static void main(String[] args) {
-		
-		
-			
-			Scanner sc = new Scanner(System.in);
-			
 			
 			TV tv = new TV(7, 20, false);
-			System.out.println("전원을 켜시겠습니까?");
-				
-			tv.power(sc.nextBoolean());
-			System.out.print("채널변경: ");
-			tv.channel(sc.nextBoolean());
-			System.out.print("볼륨변경:");
-			tv.volume(sc.nextBoolean());
 			
 			
 			tv.status();
-				
-			System.out.println(tv.toString());
 			
-			sc.close();
+			tv.power(true);
+			tv.volume(120);
+			tv.status();
+			
+			tv.volume(false);
+			tv.status();
+			
+			tv.channel(0);
+			tv.status();
+			
+			tv.channel(true);
+			tv.channel(true);
+			tv.channel(true);
+			tv.status();
+			
+			tv.power(false);
+			tv.status();
+				
+			
 			
 	}
 
