0c81335500b5b0bf644e5e44e7bf838d6114a6cd
최정필 <wjdvlf1407@gmail.com>
2022-04-25 11:16:10 +0900

    Ex10 시작

---
+++ src/com/javaex/ex10/GoodsApp.java
@@ -0,0 +1,16 @@
+package com.javaex.ex10;
+
+import com.javaex.ex09.Goods;
+
+public class GoodsApp {
+	
+	public static void main(String[] args) {
+		
+		Goods camera = new Goods("니콘", 400000);
+		Goods computer = new Goods("LG그렘", 900000);
+		Goods cup = new Goods("머그컵", 2000);
+		
+		
+	}
+
+}
