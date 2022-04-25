1bee7d391920df1e30fce988edb7b6b1bd7efd99
최정필 <wjdvlf1407@gmail.com>
2022-04-25 14:04:23 +0900

    Ex11 예제1

---
+++ src/com/javaex/ex11/Math.java
@@ -0,0 +1,21 @@
+package com.javaex.ex11;
+
+public class Math {
+	
+	//필드
+	
+	//생성자
+	
+	//메소드 - gs 
+	
+	//메소드 - 일반
+	public int plus(int a, int b) {
+		int sum = a + b;
+		return sum;
+}
+
+	public double plus  (double a, double b) {
+		double sum = a + b;
+		return sum;
+	}
+}
---
+++ src/com/javaex/ex11/MathApp.java
@@ -0,0 +1,12 @@
+package com.javaex.ex11;
+
+public class MathApp {
+
+	public static void main(String[] args) {
+		
+		Math math = new Math();
+		System.out.println(math.plus(3, 2));
+
+	}
+
+}
